d8545086e5e8329b661bb7f95683ce2bed80b821
Vladyslavmaster999 <master123456sf89@gmail.com>
2024-11-24 10:38:47 +0200

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -42,17 +42,3 @@ document.getElementById("subscription-form").addEventListener("submit", function
 });
 
 
-  <script>
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel({
-            items: 1,
-            loop: true,
-            margin: 10,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            nav: true,
-            dots: true
-        });
-    });
-  </script>
-   
