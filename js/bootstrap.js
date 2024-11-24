32dcc2bd006ad4678fbc513ad420bc1c1ac2e01c
Vladyslavmaster999 <master123456sf89@gmail.com>
2024-11-24 06:29:36 +0200

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -28,4 +28,17 @@ setInterval(updateTimer, 1000);
 // Перший запуск
 updateTimer();
 
+document.getElementById("subscription-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+
+    if (email) {
+        alert(`Дякуємо за підписку! Ваш email: ${email}`);
+        document.getElementById("email-input").value = ""; // Очищаємо поле
+    } else {
+        alert("Будь ласка, введіть дійсну адресу електронної пошти.");
+    }
+});
+
    
